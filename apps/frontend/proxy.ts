af3d895c00b8e92411245c5f6943bd4f1ec2e993
Aryan Kashyap <kashyaparyan093@gmail.com>
2026-02-06 01:38:50 +0000

    Update proxy.ts

--- apps/frontend/proxy.ts
+++ apps/frontend/proxy.ts
@@ -1,6 +1,8 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
 const isPublicRoute = createRouteMatcher([
+    '/sign-in(.*)',
+    '/create-account(.*)',
     '/sso-callback(.*)',
     '/privacy-policy(.*)',
     '/terms-of-service(.*)',

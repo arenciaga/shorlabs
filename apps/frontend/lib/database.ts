305f8ee992f0a230ab2aa3c6d40d6ca84c126810
Aryan Kashyap <kashyaparyan093@gmail.com>
2026-03-11 01:30:09 +0000

    Update database.ts

--- apps/frontend/lib/database.ts
+++ apps/frontend/lib/database.ts
@@ -1,6 +1,4 @@
-import { Label } from "radix-ui";
-
-export const ACU_OPTIONS = [,
+export const ACU_OPTIONS = [
     { value: 1, label: "Hobby", description: "2 GB RAM", minPlan: "hobby" },
     { value: 2, label: "Basic", description: "4 GB RAM", minPlan: "plus" },
     { value: 4, label: "Standard", description: "8 GB RAM", minPlan: "plus" },
